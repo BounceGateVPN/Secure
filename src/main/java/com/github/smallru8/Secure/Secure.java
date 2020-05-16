8535ab14c78582f01d3aa049b8e49afb40b0a04e
smallru8 <30855604+smallru8@users.noreply.github.com>
2020-05-16 17:07:02 +0800

    Fix : RSA method cast bug

--- src/main/java/com/github/smallru8/Secure/Secure.java
+++ src/main/java/com/github/smallru8/Secure/Secure.java
@@ -1,6 +1,12 @@
 package com.github.smallru8.Secure;
 
 import java.io.IOException;
+import java.security.interfaces.RSAKey;
+
+import org.bouncycastle.crypto.AsymmetricBlockCipher;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.bouncycastle.crypto.engines.RSAEngine;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 
 /**
  * 身分驗證
@@ -20,6 +26,44 @@ public class Secure {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * RSA2048加密
+	 * @param plaintext
+	 * @param key
+	 * @return
+	 */
+	public byte[] encryption_RSA(byte[] plaintext,AsymmetricKeyParameter key) {
+		byte[] ciphertext = null;
+		AsymmetricBlockCipher cipher = new RSAEngine();
+		cipher.init(true, key);//true表示加密
+		try {
+			ciphertext = cipher.processBlock(plaintext, 0, plaintext.length);
+		} catch (InvalidCipherTextException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ciphertext;
+	}
+	
+	/**
+	 * RSA2048解密
+	 * @param ciphertext
+	 * @param key
+	 * @return
+	 */
+	public byte[] decryptData_RSA(byte[] ciphertext,AsymmetricKeyParameter key) {
+		byte[] plaintext = null;
+        AsymmetricBlockCipher cipher = new RSAEngine();
+        cipher.init(false, key);//false表示解密
+        try {
+			plaintext = cipher.processBlock(ciphertext, 0, ciphertext.length);
+		} catch (InvalidCipherTextException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return plaintext;
 	}
 	
 }
--- src/main/java/com/github/smallru8/Secure/TEST/Test1.java
+++ src/main/java/com/github/smallru8/Secure/TEST/Test1.java
@@ -3,6 +3,9 @@ package com.github.smallru8.Secure.TEST;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
+import org.bouncycastle.crypto.util.PrivateKeyFactory;
+import org.bouncycastle.crypto.util.PublicKeyFactory;
+
 import com.github.smallru8.Secure.Secure;
 import com.github.smallru8.Secure.KeyGen.RSA;
 
@@ -15,6 +18,14 @@ public class Test1 {
 	
 	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
 		Secure sec = new Secure();
+		String in = "RSAtest";
+		
+		System.out.println("加密前 : "+in);
+		byte[] Ciphertext = sec.encryption_RSA(in.getBytes("utf-8"), PublicKeyFactory.createKey(sec.cr.getPublicKey().getEncoded()));
+		System.out.println("加密後 : "+new String(Ciphertext,"utf-8"));
+		byte[] Plaintext = sec.decryptData_RSA(Ciphertext, PrivateKeyFactory.createKey(sec.cr.getPrivateKey().getEncoded()));
+		System.out.println("解密後 : "+new String(Plaintext,"utf-8"));
+		
 	}
 	
 }
